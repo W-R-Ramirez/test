9f81d379d16f78b70ba661a05ad6bc943388d098
Ramiz Javed <ramizjaved@Ramizs-MacBook-Pro.local>
2017-09-10 20:08:01 -0500

    now it only shifts letters

--- caeser.py
+++ caeser.py
@@ -5,28 +5,26 @@ def encrypt(plaintext,shiftAmount):
     ciphertext = ''
     for letter in plaintext:
 
-        # spaces stay spaces
-        if letter == ' ':
-            ciphertext += letter
+        orderOfLetter = ord(letter)
+        # shift capital letter
+        if orderOfLetter > 64 and orderOfLetter < 91:
+            # order of 'A' is 65
+            # we first get a number between 0-25
+            orderOfCipherLetter = ((orderOfLetter-65)+shiftAmount)%26
+            # then we get the correct ascii order
+            orderOfCipherLetter += 65
+        # shift lower case
+        elif orderOfLetter > 96 and orderOfLetter < 123:
+            orderOfCipherLetter = ((orderOfLetter-97)+shiftAmount)%26
+            orderOfCipherLetter += 97
+        # everything else stays the same
         else:
-            orderOfLetter = ord(letter)
-            # shift capital letter
-            if orderOfLetter < 91:
-                # order of 'A' is 65
-                # we first get a number between 0-25
-                orderOfCipherLetter = ((orderOfLetter-65)+shiftAmount)%26
-                # then we get the correct ascii order
-                orderOfCipherLetter += 65
-            # shift lower case
-            else:
-                orderOfCipherLetter = ((orderOfLetter-97)+shiftAmount)%26
-                orderOfCipherLetter += 97
-            # get the letter from the order
-            cipherLetter = chr(orderOfCipherLetter)
+            orderOfCipherLetter = orderOfLetter
+        # get the letter from the order
+        cipherLetter = chr(orderOfCipherLetter)
 
-            ciphertext += cipherLetter
+        ciphertext += cipherLetter
     return ciphertext
 
 def decrypt(ciphertext,shiftAmount):
-    # negate the shiftAmount 
     return encrypt(ciphertext,-shiftAmount)
